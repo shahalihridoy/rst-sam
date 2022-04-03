8794ffe72536622df0c23facecf1af3182147c03
Md Shah Ali <shahalihridoy@gmail.com>
2022-04-03 14:59:07 +0600

    update code

--- .aws-sam/build/GetAllTaps/src/handlers/get-all-taps-by-id.js
+++ .aws-sam/build/GetAllTaps/src/handlers/get-all-taps-by-id.js
@@ -1,36 +1,38 @@
-const AWS = require('aws-sdk')
-const ddb = new AWS.DynamoDB.DocumentClient()
+const AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
+AWS.config.update({
+  region: "us-east-2",
+});
 
-exports.handler = async () => {
-  const { BOARDS_TABLE } = process.env
-  AWS.config.update({
-    region: 'us-east-2'
-  })
+exports.handler = async (events) => {
+  const { id: boardId } = events.pathParameters;
+  const { BOARDS_TABLE } = process.env;
   const params = {
     TableName: BOARDS_TABLE,
-    FilterExpression: '#x = :device and attribute_not_exists(#y)',
+    FilterExpression: "#x = :device and attribute_not_exists(#y)",
     ExpressionAttributeNames: {
-      '#x': 'device_id',
-      '#y': 'device_data_raw'
+      "#x": "device_id",
+      "#y": "device_data_raw",
     },
     ExpressionAttributeValues: {
-      ':device': 'test_modem2'
-    }
-  }
+      ":device": boardId,
+    },
+    Limit: 1,
+  };
 
   try {
     const data = await ddb
       .scan(params)
       .promise()
-      .then((data) => data.Items)
+      .then((data) => data.Items);
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
-    }
+      body: JSON.stringify(data),
+    };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify(error)
-    }
+      body: JSON.stringify(error),
+    };
   }
-}
+};
